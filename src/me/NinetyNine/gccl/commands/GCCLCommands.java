9b163257535a953bfbb4ff1abce97702fbf471a7
BuildTools <unconfigured@null.spigotmc.org>
2018-03-11 21:05:29 +0800

    Last commit of the day!

--- src/me/NinetyNine/gccl/commands/GCCLCommands.java
+++ src/me/NinetyNine/gccl/commands/GCCLCommands.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
